da6f4ae7104f5c3bd328601b28497e87b30eb5cf
stwind <stwindfy@gmail.com>
2018-10-29 21:28:00 +0800

    Update ui

--- packages/ca/src/ca.ts
+++ packages/ca/src/ca.ts
@@ -1,3 +1,4 @@
+import { getIn } from '@thi.ng/paths';
 import { button } from '@thi.ng/hdom-components/button';
 import { gestureStream, GestureType } from "@thi.ng/rstream-gestures";
 import { filter } from "@thi.ng/transducers/xform/filter";
@@ -45,23 +46,32 @@ const makeSlider = () => {
     [slider_, { attribs: ui.slider, ...args }, value];
 };
 
-export const ca = ({ ui, bus, views }: AppContext) => {
+export const ca = ({ ui, bus, views: { params, presetOpts, preset } }: AppContext) => {
   const app = new CA();
   const canvas_ = makeCanvas(app);
 
   const cbtn = button({ attribs: ui.cbutton });
-  const cslider = makeSlider();
+
+  const setParam = name => val => bus.dispatch([ev.SET_PARAM, [name, val]]);
+
+  const s1 = makeSlider();
+  const s2 = makeSlider();
+  const s3 = makeSlider();
 
   const setPreset = v => bus.dispatch([ev.SET_PRESET, v]);
-  const setParam = name => val => bus.dispatch([ev.SET_PARAM, [name, val]]);
+
+  const paramSlider = (name, s) =>
+    [name, [s, { min: 0, max: 8, step: 1, onchange: setParam(name) }, getIn(params.deref(), name)]];
 
   return () =>
     ['div', ui.root,
       [canvas_, ui.ca],
       [panel, ui.panel,
         ['param1', [cbtn, {}, 'fuck'], [cbtn, {}, 'You']],
-        ['param4', [select_, { onchange: setPreset }, views.presetOpts.deref(), views.preset.deref()]],
-        ['e1', [cslider, { min: 0, max: 8, step: 1, onchange: setParam('e1') }, views.params.deref().e1]],
+        ['preset', [select_, { onchange: setPreset }, presetOpts.deref(), preset.deref()]],
+        paramSlider('e1', s1),
+        paramSlider('e2', s2),
+        paramSlider('f1', s3)
       ]
     ];
 };
