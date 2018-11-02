7e8c8ec4a46c41f23171ae3edd553a349a44b6e5
stwind <stwindfy@gmail.com>
2018-11-02 14:55:36 +0800

    Add torus

--- packages/cube/src/scene/index.ts
+++ packages/cube/src/scene/index.ts
@@ -1,11 +1,8 @@
 import { m4 } from 'twgl.js';
-import * as bunny from 'bunny';
+import createTorusMesh from 'primitive-torus';
 import { flatten } from '@thi.ng/iterators/flatten';
 
-import {
-  Application, Scene, Mesh, Material, Command,
-  // Cube
-} from '@pngu/gl';
+import { Application, Scene, Mesh, Material, Command, Geometry } from '@pngu/gl';
 import { Camera } from '@pngu/gl/camera';
 
 import vert from './vert.glsl';
@@ -24,17 +21,17 @@ export class App extends Application {
     this.camera.aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
     this.camera.near = 0.5;
     this.camera.far = 100;
-    this.camera.position = [1, 4, -46];
+    this.camera.position = [1, 4, -10];
     this.camera.target = [0, 0, 0];
     this.camera.up = [0, 1, 0];
 
-    // const cube = new Cube(1);
-    const cube = {
-      attributes: {
-        position: [...flatten(bunny.positions)],
-        indices: [...flatten(bunny.cells)]
-      }
-    };
+    const attribs = createTorusMesh();
+    const cube = new Geometry({
+      position: [...flatten(attribs.positions)],
+      indices: [...flatten(attribs.cells)],
+      uv: [...flatten(attribs.uvs)],
+      normal: [...flatten(attribs.normals)]
+    });
     this.mat = new Material(vert, frag, {
       model: m4.identity(),
       view: this.camera.view,
--- packages/cube/src/scene/vert.glsl
+++ packages/cube/src/scene/vert.glsl
@@ -7,6 +7,13 @@ uniform mat4 model;
 uniform mat4 view;
 
 attribute vec3 position;
+// attribute vec3 normal;
+// attribute vec2 uv;
+
+// varying vec3 vVertPos;
+// varying vec3 vLightPos;
+// varying vec3 vNormal;
+// varying vec2 vUv;
 
 void main () {
   gl_Position = proj * view * model * vec4(position, 1.0);
