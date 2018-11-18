a4692c63af51a296b2cee8581a460f42d5d39348
stwind <stwindfy@gmail.com>
2018-11-18 13:11:51 +0800

    Update cam ui

--- packages/gl/src/camera-ui.ts
+++ packages/gl/src/camera-ui.ts
@@ -1,8 +1,8 @@
 import { gestureStream, GestureType, GestureInfo } from "@thi.ng/rstream-gestures";
 import { ISubscribable } from "@thi.ng/rstream/api";
-import { quat } from 'gl-matrix';
 import { Vec2 } from '@thi.ng/vectors/vec2';
 import { Vec3, setS3 } from '@thi.ng/vectors/vec3';
+import { quat } from 'gl-matrix';
 
 import { PerspectiveCamera } from './Camera';
 
@@ -35,8 +35,11 @@ export class CameraUI {
   private center: Vec2 = new Vec2();
   private up: Vec3 = new Vec3();
   private viewDir: Vec3 = new Vec3();
-  private clickPos: Vec3;
+  private clickPos: Vec3 = new Vec3();
+  private delta: Vec3 = new Vec3();
 
+  private side: Vec3 = new Vec3();
+  private axis: Vec3 = new Vec3();
   private q: quat = quat.create();
   private vd: Vec3 = new Vec3();
   private u: Vec3 = new Vec3();
@@ -60,19 +63,18 @@ export class CameraUI {
   }
 
   private handleStart({ pos }: GestureInfo) {
-    this.clickPos = this.spherePos(pos[0], pos[1]);
-    this.up = this.camera.up.copy();
-    this.viewDir = this.camera.position.copy().sub(this.camera.target);
+    this.spherePos(this.clickPos, pos[0], pos[1]);
+    this.up.set(this.camera.up);
+    this.viewDir.set(this.camera.position).sub(this.camera.target);
   }
 
   private handleDrag({ pos, delta }: GestureInfo) {
-    const curPos = this.spherePos(delta[0] + pos[0], delta[1] + pos[1]);
+    const d = this.spherePos(this.delta, delta[0] + pos[0], delta[1] + pos[1]).sub(this.clickPos);
 
-    const side = this.up.copy().cross(this.viewDir).normalize().mulN(curPos[0] - this.clickPos[0]);
-    const axis = this.up.copy().mulN(curPos[1] - this.clickPos[1]).add(side).cross(this.viewDir).normalize();
-    const angle = new Vec3([curPos[0] - this.clickPos[0], curPos[1] - this.clickPos[1], 0]).magSq();
+    this.side.set(this.up).cross(this.viewDir).normalize().mulN(d[0]);
+    this.axis.set(this.up).mulN(d[1]).add(this.side).cross(this.viewDir).normalize();
 
-    quat.setAxisAngle(this.q, axis, angle * this.speed);
+    quat.setAxisAngle(this.q, this.axis, d.magSq() * this.speed);
     transformQuat(this.vd, this.viewDir, this.q);
     transformQuat(this.u, this.up, this.q);
 
@@ -80,16 +82,10 @@ export class CameraUI {
     this.camera.position = this.camera.target.addNew(this.vd);
   }
 
-  spherePos(x: number, y: number): Vec3 {
-    const vx = (x - this.center[0]) / this.radius;
-    const vy = -(y - this.center[1]) / this.radius;
-    const v = new Vec3([vx, vy, 0]);
+  spherePos(v: Vec3, x: number, y: number): Vec3 {
+    v.setS((x - this.center[0]), -(y - this.center[1]), 0).divN(this.radius);
     const mag = v.magSq()
-    if (mag > 1.0) {
-      return v.normalize();
-    } else {
-      return v.setS(vx, vy, Math.sqrt(1.0 - mag));
-    }
+    return mag > 1.0 ? v.normalize() : v.setS(v[0], v[1], Math.sqrt(1.0 - mag));
   }
 
   release() {
